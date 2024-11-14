ae03a317a8bcd78ca6ca77c6fffe3596cff6a5b1
Andrew Higgins <ahiggins8434@gmail.com>
2024-11-14 14:42:17 -0500

    updates

--- install/install-git.sh
+++ install/install-git.sh
@@ -1,4 +1,9 @@
 #!/bin/sh
 
 mkdir -p $CONFIG/git
-ln -sf $DOTFILES/git/gitconfig $CONFIG/git/config
+ln -sf $DOTFILES/git/gitconfig $CONFIG/git/config.template
+
+if [ ! -f $CONFIG/git/config ]; then
+  echo "[include]" > $CONFIG/git/config
+  echo "  path = $CONFIG/git/config.template" >> $CONFIG/git/config
+fi
